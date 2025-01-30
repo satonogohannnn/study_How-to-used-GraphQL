1d07c99197395cd7f6471895cbe3664b6b48d408
satonogohannnn <tosshy.0513@gmail.com>
2025-01-31 03:45:53 +0900

    Add addTodo and updateTodo mutations with corresponding handlers in App component

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { Button } from './components/ui/button';
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -11,6 +11,26 @@ const client = new ApolloClient({
 const GET_TODOS = gql`
   query {
     getTodos {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const ADD_TODO = gql`
+  mutation AddTodo($title: String!) {
+    addTodo(title: $title) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const UPDARE_TODO = gql`
+  mutation UpdateTodo($id: ID!, $completed: Boolean!) {
+    updateTodo(id: $id, completed: $completed) {
       id
       title
       completed
@@ -28,11 +48,29 @@ function App() {
   const { loading, error, data } = useQuery(GET_TODOS, {
     fetchPolicy: "network-only",
   });
+  const [addTodo] = useMutation(ADD_TODO);
+  const [updateTodo] = useMutation(UPDARE_TODO);
+  const [title, setTitle] = useState("");
 
   const todos = data ? data.getTodos : [];
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+
+  const handleAddTodo = async () => {
+    await addTodo({
+      variables: { title },
+      refetchQueries: [{ query: GET_TODOS }]
+    });
+    setTitle("");
+  }
+
+  const handleUpdateTodo = async (id: string, completed: boolean) => {
+    await updateTodo({
+      variables: { id, completed: !completed },
+      refetchQueries: [{ query: GET_TODOS }]
+    })
+  }
 
   return (
     <>
@@ -48,7 +86,11 @@ function App() {
                 textDecoration: todo.completed ? "line-through" : "none",
               }}
             >
-              <input type="checkbox" checked={todo.completed} />
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleUpdateTodo(todo.id, todo.completed)}
+              />
               {todo.title}
             </li>
           ))}
