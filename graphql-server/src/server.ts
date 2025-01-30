23e7f2623c8ce4af3ea2c27579602874ef8c8b04
satonogohannnn <tosshy.0513@gmail.com>
2025-01-31 02:47:12 +0900

    Add addTodo mutation to GraphQL server

--- graphql-server/src/server.ts
+++ graphql-server/src/server.ts
@@ -23,13 +23,29 @@ const typeDefs = gql`
   type Query {
     getTodos: [Todo!]!
   }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
+  }
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos,
   },
-};
+
+  Mutation: {
+    addTodo: (_: unknown, { title }: { title: string }) => {
+        const newTodo = {
+            id: String(todos.length + 1),
+            title,
+            completed: false,
+        };
+        todos.push(newTodo);
+        return newTodo;
+    }
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
