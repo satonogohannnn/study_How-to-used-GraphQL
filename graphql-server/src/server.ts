669c87124e0f5b918e2f86c7c9fe07325ba1fce7
satonogohannnn <tosshy.0513@gmail.com>
2025-01-31 02:40:31 +0900

    Add Todo type and getTodos query to GraphQL server

--- graphql-server/src/server.ts
+++ graphql-server/src/server.ts
@@ -1,14 +1,33 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const todos = [
+  {
+    id: "1",
+    title: "GraphQLを勉強する",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Reactを勉強する",
+    completed: false,
+  },
+];
+
 const typeDefs = gql`
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+  }
+
   type Query {
-    message: String
+    getTodos: [Todo!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    message: () => "Hello World!",
+    getTodos: () => todos,
   },
 };
 
